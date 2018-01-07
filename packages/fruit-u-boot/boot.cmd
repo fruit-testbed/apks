085cb486e20288b5b7075f7a99353e475b4c5d17
Herry <herry13@gmail.com>
2018-01-07 12:33:57 +0000

    enabled loading boot params from cmdline.txt

--- packages/fruit-u-boot/boot.cmd
+++ packages/fruit-u-boot/boot.cmd
@@ -27,13 +27,14 @@ echo "=== Saving boot root device to boot-root.env ===";
 mw ${env_addr_r} 0x20 0x100;
 env export -t ${env_addr_r} root_dev;
 fatwrite mmc 0:1 ${env_addr_r} ${env_file} 24;
-echo "=== Setting boot arguments ===";
-setenv bootargs_default "8250.nr_uarts=1 console=ttyAMA0,115200 console=tty1 cgroup_enable=cpuset noquite panic=5 loglevel=7 dwc_otg.lpm_enable=0 root=${root_dev} rootfstype=ext4";
-if fatload mmc 0:1 ${env_addr_r} nooverlaytmpfs;
+echo "=== Setting default boot parameters ===";
+setenv bootargs "8250.nr_uarts=1 console=ttyAMA0,115200 console=tty1 cgroup_enable=cpuset noquite panic=5 loglevel=7 dwc_otg.lpm_enable=0 root=${root_dev} rootfstype=ext4";
+env export -t 0x100 bootargs;
+if fatload mmc 0:1 0x109 cmdline.txt;
 then
-	setenv bootargs ${bootargs_default};
-else
-	setenv bootargs "${bootargs_default} overlaytmpfs";
+	env import -t 0x100;
+	setenv bootargs "${bootargs} 8250.nr_uarts=1 cgroup_enable=cpuset panic=5 loglevel=7";
+	echo "=== Loaded boot parameters from cmdline.txt";
 fi;
 echo "=== Loading kernel and initramfs ===";
 ext4load mmc ${root_part} ${kernel_addr_r} ${boot_prefix}/${kernel};
