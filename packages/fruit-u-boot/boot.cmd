a1876e246d60ccc28b5ba7e19c0eb7700312b132
Herry <herry13@gmail.com>
2018-01-20 00:39:56 +0000

    fixed U-Boot bootargs

--- packages/fruit-u-boot/boot.cmd
+++ packages/fruit-u-boot/boot.cmd
@@ -28,13 +28,14 @@ mw ${env_addr_r} 0x20 0x100;
 env export -t ${env_addr_r} root_dev;
 fatwrite mmc 0:1 ${env_addr_r} ${env_file} 24;
 echo "=== Setting default boot parameters ===";
-setenv bootargs "8250.nr_uarts=1 console=ttyAMA0,115200 console=tty1 cgroup_enable=cpuset noquite panic=5 loglevel=7 dwc_otg.lpm_enable=0 root=${root_dev} rootfstype=ext4";
+setenv bootargs_default "8250.nr_uarts=1 console=ttyAMA0,115200 console=tty1 noquite cgroup_enable=cpuset panic=5 loglevel=7 dwc_otg.lpm_enable=0 root=${root_dev} rootfstype=ext4 overlaytmpfs"
+setenv bootargs "${bootargs_default}";
 env export -t 0x100 bootargs;
 if fatload mmc 0:1 0x109 cmdline.txt;
 then
 	env import -t 0x100;
-	setenv bootargs "${bootargs} 8250.nr_uarts=1 cgroup_enable=cpuset panic=5 loglevel=7";
-	echo "=== Loaded boot parameters from cmdline.txt";
+	setenv bootargs "${bootargs_default} ${bootargs}";
+	echo "=== Loaded boot parameters from cmdline.txt ===";
 fi;
 echo "=== Loading kernel and initramfs ===";
 ext4load mmc ${root_part} ${kernel_addr_r} ${boot_prefix}/${kernel};
