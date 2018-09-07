13794b6b2abd639bffb2ff095f3a99745b0d25dd
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-09-07 14:32:46 +0100

    Fix clean targets by properly propagating arguments through to docker

--- build-via-docker.sh
+++ build-via-docker.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 DOCKER_ARCH=${DOCKER_ARCH:-armhf}
-exec \
-    docker run -it --rm -v $(realpath $(dirname "$0")/..):/media/data/fruitos \
+
+cd "$(dirname "$0")"
+
+tmpscript=$(mktemp tmpscript.XXXXXXXXXX)
+trap "rm -f $tmpscript" 0
+
+cat > $tmpscript <<EOF
+set -xe
+apk update
+apk add make git
+cd /media/data/fruitos/apks
+rm -f .prepare
+make $@
+EOF
+
+docker run -it --rm -v $(realpath ..):/media/data/fruitos \
     multiarch/alpine:${DOCKER_ARCH}-edge \
-    /bin/sh -c 'apk update && apk add make git && cd /media/data/fruitos/apks && rm -f .prepare && make '"$@"
+    /bin/sh -c "cd /media/data/fruitos/apks && sh $tmpscript"
