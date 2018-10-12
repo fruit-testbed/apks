0d90b7a1d38f0d931b95d67e7dc5d02ca2ac0528
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-10-12 21:59:13 +0100

    Support for caching HTTP proxy for alpine etc.

--- build-via-docker.sh
+++ build-via-docker.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 DOCKER_ARCH=${DOCKER_ARCH:-armhf}
 
+# If you run:
+#   docker run --rm --name squid adricu/alpine-squid
+# and set DOCKER_SQUID_CONTAINER=squid
+# then you can avoid repeated downloads of a lot of alpine packages.
+#
+DOCKER_SQUID_CONTAINER=${DOCKER_SQUID_CONTAINER:-""}
+if [ "$DOCKER_SQUID_CONTAINER" = "" ]; then
+    docker_squid_opts=""
+else
+    docker_squid_opts="--link ${DOCKER_SQUID_CONTAINER}:squid -e http_proxy=http://squid:3128/"
+fi
+
 cd "$(dirname "$0")"
 
 tmpscript=$(mktemp tmpscript.XXXXXXXXXX)
@@ -16,5 +28,6 @@ make $@
 EOF
 
 docker run -it --rm -v $(realpath ..):/media/data/fruitos \
+    ${docker_squid_opts} \
     multiarch/alpine:${DOCKER_ARCH}-edge \
     /bin/sh -c "cd /media/data/fruitos/apks && sh $tmpscript"
